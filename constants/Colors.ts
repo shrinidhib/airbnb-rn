761aedcc64458936ce6760c3680489a01da83fd2
Shrinidhi <b.shrinidhi1214@gmail.com>
2024-02-13 21:19:40 +0530

    first commit: created tabs and pages layout

---
+++ constants/Colors.ts
@@ -0,0 +1,16 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+
+export default {
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: tintColorLight,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorLight,
+  },
+  
+  primary: '#ff385c',
+  grey: '#5e5d5e',
+  dark: '#1a1a1a',
+};
